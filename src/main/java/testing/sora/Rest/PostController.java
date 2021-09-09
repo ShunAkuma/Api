7f4d8f5ebbdff54bf674344d2dcfbfacfb059440
Rise <wasbornunnamed@gmail.ru>
2021-09-09 10:27:28 +0300

    Add delete and update to postController

--- src/main/java/testing/sora/Rest/PostController.java
+++ src/main/java/testing/sora/Rest/PostController.java
@@ -2,6 +2,7 @@ package testing.sora.Rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import testing.sora.Exception.UserDoesNotExistException;
 import testing.sora.Model.post;
 import testing.sora.Service.PostService;
 
@@ -15,5 +16,21 @@ public class PostController {
     @PostMapping("/newpost")
     public ResponseEntity createPost(@RequestBody post post) throws Exception {
         return ResponseEntity.ok(postService.createPost(post));
+    }
+
+    @GetMapping(value = "/post/{lead_id}")
+    public ResponseEntity GetAllPost(@PathVariable Long lead_id) throws UserDoesNotExistException {
+        return ResponseEntity.ok(postService.getAllPostFromLead(lead_id));
+    }
+
+//    @PutMapping("/change/{post_id}")
+//    public ResponseEntity changePost(@PathVariable Long post_id){
+//
+//    }
+
+    @DeleteMapping(value = "/delete/Post/{post_id}/delete")
+    public ResponseEntity deletePost(@PathVariable Long post_id){
+        postService.deletePost(post_id);
+        return ResponseEntity.ok("Post was be deleted");
     }
 }
--- src/main/java/testing/sora/Service/PostService.java
+++ src/main/java/testing/sora/Service/PostService.java
@@ -1,10 +1,14 @@
 package testing.sora.Service;
 
 import org.springframework.stereotype.Service;
+import testing.sora.Exception.NoSuchPostExists;
+import testing.sora.Exception.UserDoesNotExistException;
 import testing.sora.Model.lead;
 import testing.sora.Model.post;
 import testing.sora.Repository.PostRepository;
 import testing.sora.Repository.UserRepository;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -21,5 +25,30 @@ public class PostService {
             throw new Exception("Пользователь с таким именем не найден!");
         }
         return postRepository.save(post);
+    }
+
+    public List<post> getAllPostFromLead(Long lead_id) throws UserDoesNotExistException {
+        if (userRepository.findById(lead_id) == null){
+            throw new UserDoesNotExistException("No such user exists");
+        }
+        return postRepository.findAllByLead_id(lead_id);
+    }
+
+    public void changePost(Long post_id) throws NoSuchPostExists {
+        if (postRepository.findById(post_id) == null){
+            throw new NoSuchPostExists("No such post exist");
+        }
+//        post posts = new post();
+//        posts = postRepository.findById(post_id);
+
+
+    }
+
+    public void deletePost(Long post_id) throws NoSuchPostExists {
+        if (postRepository.findById(post_id) == null){
+            throw new NoSuchPostExists("No such post exists");
+            post postDel = postRepository.findById(post_id);
+            postRepository.delete(postDel);
+        }
     }
 }
