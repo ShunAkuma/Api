7081108efa0e8e3008bbf5c655cb198685b677cf
Rise <wasbornunnamed@gmail.ru>
2021-09-09 13:55:48 +0300

    Validation data

--- src/main/java/testing/sora/Model/InputDto/LeadLoginInputModel.java
+++ src/main/java/testing/sora/Model/InputDto/LeadLoginInputModel.java
@@ -4,10 +4,18 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @Getter
 public class LeadLoginInputModel {
+
+    @NotNull
+    @Size(min = 6,max = 20)
     private String login;
+    @NotNull
+    @Size(min = 8,max = 20)
     private String password;
 
     public LeadLoginInputModel() {
